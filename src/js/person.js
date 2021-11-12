4b1aa38733e70d42a792f53b61aec1b0b99419da
Ryan Ashby <rjapsu160@gmail.com>
2021-11-12 12:32:15 -0800

    add code for life expect on jupiter. PASS.

--- src/js/person.js
+++ src/js/person.js
@@ -50,5 +50,9 @@ export default class Person {
       goldenYears = ((maleExpect - this.age)/(1.88));
       return parseFloat(goldenYears.toFixed(1));
     }; 
+    if ((this.gender === "male") && this.planet === "jupiter") {
+      goldenYears = ((maleExpect - this.age)/(11.86));
+      return parseFloat(goldenYears.toFixed(1));
+    }; 
   };
 };
