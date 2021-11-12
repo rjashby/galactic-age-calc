94b4e88fb863b75cd6d6c013fb95e85333e920e4
Ryan Ashby <rjapsu160@gmail.com>
2021-11-12 09:27:16 -0800

    add 'planet' as a property of Person. re-test/pass.

--- __tests__/person.test.js
+++ __tests__/person.test.js
@@ -3,9 +3,9 @@ import Person from "../src/js/person";
 describe("Person", () => {
 
   test("should correctly create a Person object with properties of age and gender", () => {
-    const person1 = new Person(21, "male")
+    const person1 = new Person(21, "male", "earth")
     expect(person1.age).toEqual(21);
     expect(person1.gender).toEqual("male");
-    
+    expect(person1.planet).toEqual("earth");
   });
 })
--- src/js/person.js
+++ src/js/person.js
@@ -1,6 +1,10 @@
+let maleExpect = 75.1
+let femaleExpect = 80.5
+
 export default class Person {
-  constructor(age, gender) {
+  constructor(age, gender, planet) {
     this.age = age
     this.gender = gender
+    this.planet = planet
   }
 }
