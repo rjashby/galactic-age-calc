c33ca2519cbbd468f2bf1dfe08ec55c91a3a30e8
Ryan Ashby <rjapsu160@gmail.com>
2021-11-12 12:17:07 -0800

    add code for life expect on mercury. PASS.

--- src/js/person.js
+++ src/js/person.js
@@ -38,6 +38,9 @@ export default class Person {
       goldenYears = (maleExpect - this.age);
       return parseFloat(goldenYears.toFixed(1));
     };
-     
+    if ((this.gender === "male") && this.planet === "mercury") {
+      goldenYears = ((maleExpect - this.age)/(.24));
+      return parseFloat(goldenYears.toFixed(1));
+    }; 
   };
 };
